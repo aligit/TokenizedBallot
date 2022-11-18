70a0335db85dc59a1daf2352719e466ce20beb18
alifakheri <alifakheri@protonmail.com>
2022-11-18 15:01:42 +0100

    Enables Burnable in order to implement the appropriate test case

--- contracts/MyERC20.sol
+++ contracts/MyERC20.sol
@@ -2,9 +2,10 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract MyERC20Token is ERC20, AccessControl {
+contract MyERC20Token is ERC20, ERC20Burnable, AccessControl {
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
     constructor() ERC20("MyToken", "MTK") {
--- contracts/MyERC721.sol
+++ contracts/MyERC721.sol
@@ -2,9 +2,10 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract MyERC721Token is ERC721, AccessControl {
+contract MyERC721Token is ERC721, ERC721Burnable, AccessControl {
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
     constructor() ERC721("MyToken", "MTK") {
