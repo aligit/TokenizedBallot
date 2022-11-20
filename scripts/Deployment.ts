cb5c9899e366dbc70ae8cdc1e4a621096e5e08a4
alifakheri <alifakheri@protonmail.com>
2022-11-20 18:36:39 +0100

    shows balance after mint

--- scripts/Deployment.ts
+++ scripts/Deployment.ts
@@ -1,17 +1,29 @@
 import { ethers } from "hardhat";
 import { MyToken__factory } from "../typechain-types";
+// import * as dotenv from 'dotenv'
+// dotenv.config()
+//
+const TEST_MINT_VALUE = ethers.utils.parseEther("10");
 
 async function main() {
-  const accounts = ethers.getSigners();
-  //For the weekend project(requires dotenv)
-  //const accounts = ethers.Wallet.fromMnemonic ();
-  const contractFactory = new MyToken__factory(accounts[0]);
+  const accounts = await ethers.getSigners();
+  const [minter, voter, other] = accounts;
+  //For the weekend project(requires dotenu)//Check Lesson8 for inspiration
+  // const wallet = new ethers.Wallet(process.env.PRIVATE_KEY ?? "");
+  const contractFactory = new MyToken__factory(minter);
   const contract = await contractFactory.deploy();
   await contract.deployed();
-  console.log(`Tokenized Votes contract deployed at ${contract.address}`)
+  console.log(`Tokenized Votes contract deployed at ${contract.address}\n`)
+  let voterTokenBalance = await contract.balanceOf(voter.address);
+  console.log(`The voter starts with at ${voterTokenBalance} decimals of balance\n`)
+  const mintTx = await contract.mint(voter.address, TEST_MINT_VALUE);
+  await mintTx.wait();
+  voterTokenBalance = await contract.balanceOf(voter.address);
+  console.log(`after the mint, the voter has ${voterTokenBalance} decimals of balance\n`)
+
 }
 
-main().catch(err) => {
-  console.error(err);
+main().catch((error) => {
+  console.error(error);
   process.exitCode = 1;
-}
+});
