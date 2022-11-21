91ac96aedafc8d2159d6160b3de653a48bb65fbb
alifakheri <alifakheri@protonmail.com>
2022-11-21 17:21:26 +0100

    Enables Delegation to other people

---
+++ scripts/Delegate.ts
@@ -0,0 +1,36 @@
+
+import { ethers } from "hardhat";
+import { MyToken, MyToken__factory } from "../typechain-types";
+import * as dotenv from 'dotenv'
+dotenv.config()
+
+const TEST_MINT_VALUE = ethers.utils.parseEther("10");
+
+async function main() {
+  const provider = ethers.getDefaultProvider("goerli")
+  const wallet = new ethers.Wallet(process.env.PRIVATE_KEY ?? "");
+  const signer = wallet.connect(provider);
+  const balanceBN = await signer.getBalance();
+  console.log(`Connected to the acount of address ${signer.address}\nThis account has a balance of ${balanceBN.toString()} Wei`);
+  const args = process.argv;
+  const params = args.slice(2);
+  if (params.length <= 0) throw new Error("arguments are missing");
+  const contractAddress = params[0];
+  const newDelegatedAddress = params[1];
+
+  console.log(`Attaching MyToken to contract`);
+  let tokenizedBallotContract: MyToken;
+  const tokenizedBallotContractFactory = new MyToken__factory(signer);
+  tokenizedBallotContract = tokenizedBallotContractFactory.attach(contractAddress);
+
+  console.log("Delegating voting power to other");
+  const delegateTx = await tokenizedBallotContract.delegate(newDelegatedAddress);
+  await delegateTx.wait();
+  let votePower = await tokenizedBallotContract.getVotes(newDelegatedAddress);
+  console.log(`Voting power delegated to: ${newDelegatedAddress} with voting power of ${votePower}`);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
