f1c64d4defe740d56bd34b10e0887e4aceb46ffb
alifakheri <alifakheri@protonmail.com>
2022-11-26 22:06:44 +0100

    Implements Delegate

--- scripts/Delegate.ts
+++ scripts/Delegate.ts
@@ -1,7 +1,8 @@
-
 import { ethers } from "hardhat";
 import { GroupTenToken, GroupTenToken__factory } from "../typechain-types";
 import * as dotenv from 'dotenv'
+import voters from './assets/voters.json'
+
 dotenv.config()
 
 async function main() {
@@ -11,21 +12,28 @@ async function main() {
   const balanceBN = await signer.getBalance();
   console.log(`Connected to the acount of address ${signer.address}\nThis account has a balance of ${balanceBN.toString()} Wei`);
   const args = process.argv;
-  const params = args.slice(2);
-  if (params.length <= 0) throw new Error("arguments are missing");
-  const contractAddress = params[0];
-  const newDelegatedAddress = params[1];
+  const contractAddress = process.env.CONTRACT;
+  const delegateeAddress = args[1];
+  if (contractAddress === undefined || contractAddress === '') {
+    throw "make sure CONTRACT address is set in the .env file";
+  }
+  if (!voters.includes(signer.address)) {
+    throw "You are not given right to vote";
+  }
 
-  console.log(`Attaching MyToken to contract`);
+  console.log(`Attaching GTET token to contract`);
   let tokenizedBallotContract: GroupTenToken;
   const tokenizedBallotContractFactory = new GroupTenToken__factory(signer);
   tokenizedBallotContract = tokenizedBallotContractFactory.attach(contractAddress);
 
-  console.log("Delegating voting power to other");
-  const delegateTx = await tokenizedBallotContract.delegate(newDelegatedAddress);
+  console.log(`Delegating voting right from ${signer.address} to ${delegateeAddress}`);
+  const delegateTx = await tokenizedBallotContract.delegate(delegateeAddress);
   await delegateTx.wait();
-  let votePower = await tokenizedBallotContract.getVotes(newDelegatedAddress);
-  console.log(`Voting power delegated to: ${newDelegatedAddress} with voting power of ${votePower}`);
+  const votePower = await tokenizedBallotContract.getVotes(delegateeAddress);
+  console.log(`Voting power delegated to: ${delegateeAddress} with voting power of ${votePower}`);
+  voters.splice(voters.indexOf(signer.address));
+  voters.push(delegateeAddress);
+  //TODO update json file with new value
 }
 
 main().catch((error) => {
