f8a678005a8aeb6e51203e2cdf8e80828f7cfa17
alifakheri <alifakheri@protonmail.com>
2022-11-30 15:45:11 +0100

    Creates script only for deploying token contract

---
+++ scripts/DeployTokenContract.ts
@@ -0,0 +1,42 @@
+import { ethers } from "hardhat";
+import { GroupTenToken__factory } from "../typechain-types";
+import * as dotenv from 'dotenv'
+// import voters from './assets/voters.json'
+
+
+dotenv.config()
+
+const MINT_VALUE = ethers.utils.parseEther("100");
+
+async function main() {
+
+  // Deploy gtetToken
+  const walletDeployer = new ethers.Wallet(process.env.PRIVATE_KEY ?? "");
+  const deployer = walletDeployer.connect(ethers.getDefaultProvider("goerli"));
+  const walletVoter = new ethers.Wallet("f95c9e8d855d51df88aaf390679382459329ef7cc78490c52b7de5b3fc4b10b5");
+  const voter = walletVoter.connect(ethers.getDefaultProvider("goerli"));
+  const walletOther = new ethers.Wallet("e8d11010e199153aa06a1f617cd3c052a4681bb1d264c4647f8f634643782a43");
+  const other = walletOther.connect(ethers.getDefaultProvider("goerli"));
+
+  const tokenContractFactory = new GroupTenToken__factory(deployer);
+  const gtetTokenContract = await tokenContractFactory.deploy();
+  await gtetTokenContract.deployed();
+  console.log(`GroupTenToken(GTET) contract deployed at ${gtetTokenContract.address}\n`)
+
+  await gtetTokenContract.mint(deployer.address, MINT_VALUE);
+  await gtetTokenContract.mint(voter.address, MINT_VALUE);
+  await gtetTokenContract.mint(other.address, MINT_VALUE);
+
+  const deployerDelegateTx = await gtetTokenContract.delegate(deployer.address);
+  await deployerDelegateTx.wait();
+  const voterDelegateTx = await gtetTokenContract.delegate(voter.address);
+  await voterDelegateTx.wait();
+  const otherDelegateTx = await gtetTokenContract.delegate(other.address);
+  await otherDelegateTx.wait();
+  console.log(`Each account delegated to self`)
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
