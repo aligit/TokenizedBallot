e5dbacd7072873b25e2aedb317d273305edaf956
alifakheri <alifakheri@protonmail.com>
2022-12-03 07:58:37 +0100

    Makes some improvements

--- scripts/CastVote.ts
+++ scripts/CastVote.ts
@@ -1,46 +1,36 @@
 import { ethers } from "hardhat";
 import * as dotenv from 'dotenv'
-import abiTokenizedBallot from "../artifacts/contracts/TokenizedBallot.sol/Ballot.json";
+import abiTokenizedBallot from "../artifacts/contracts/TokenizedBallot.sol/TokenizedBallot.json";
 import abiGroupTenToken from "../artifacts/contracts/ERC20Votes.sol/GroupTenToken.json";
-
 
 const tokenizedBallotContractABI = abiTokenizedBallot.abi;
 const groupTenTokenABI = abiGroupTenToken.abi;
-
-// expect voting with 60% of total supply vote power
-// const MIN_VOTE_POWER_VALUE = ethers.utils.parseEther("600000");
-const MIN_VOTE_POWER_VALUE = ethers.utils.parseEther("7");
-// 1000000000000000000000000
 
 dotenv.config()
 
 async function main() {
   const args = process.argv;
   const voteIndex = parseInt(args[2]);
+  const voteAmount = ethers.utils.parseEther(args[3]);
+  console.log(`voteIndex=${voteIndex}`)
+  console.log(`voteAmount=${ethers.utils.formatEther(voteAmount)}`)
   const ballotContractAddress = process.env.CONTRACT_BALLOT;
   const tokenContractAddress = process.env.CONTRACT_GTET;
-  const privateKey = process.env.PRIVATE_KEY ?? ""
+  const alchemyProvider = new ethers.providers.AlchemyProvider("goerli", process.env.ALCHEMY_API_KEY);
+  const wallet = new ethers.Wallet(process.env.PRIVATE_KEY ?? "", alchemyProvider);
+  const voter = wallet.connect(alchemyProvider);
   if (!isNumber(voteIndex)) {
     throw "Only a number representing a proposal name is accepted";
   }
+  // if (!isNumber(voteAmount)) {
+  //   throw "Only a number for vote amount is accepted";
+  // }
   if (ballotContractAddress === undefined || ballotContractAddress === '') {
     throw "make sure CONTRACT_BALLOT address is set in the .env file";
   }
   if (tokenContractAddress === undefined || tokenContractAddress === '') {
     throw "make sure CONTRACT_BALLOT address is set in the .env file";
   }
-
-  const provider = ethers.getDefaultProvider("goerli")
-  const wallet = new ethers.Wallet(privateKey);
-  const voter = wallet.connect(provider);
-  console.log(`Connected to the acount of address ${voter.address}`);
-
-  console.log(`attaching Ballot Contract`);
-  const tokenizedBallotContract = new ethers.Contract(
-    ballotContractAddress,
-    tokenizedBallotContractABI,
-    voter
-  );
 
   console.log(`attaching token Contract`);
   const groupTenTokenContract = new ethers.Contract(
@@ -49,9 +39,12 @@ async function main() {
     voter
   );
 
-
-  const votePower = await groupTenTokenContract.getVotes(voter.address);
-  console.log(`after the self delegation the voter has ${votePower} decimals of vote Power\n`)
+  console.log(`attaching Ballot Contract`);
+  const tokenizedBallotContract = new ethers.Contract(
+    ballotContractAddress,
+    tokenizedBallotContractABI,
+    voter
+  );
 
   const tProposals = await tokenizedBallotContract.getProposals();
   // List proposals
@@ -59,11 +52,16 @@ async function main() {
     console.log(`Error: ${voteIndex} is not a valid vote. Pick among the following`);
     tProposals.forEach((p: any, i: any) => console.log(`${i + 1} for ${ethers.utils.parseBytes32String(p.name)}`))
     throw "Program exiting";
+  }
 
+  const voterPastVotePower = await groupTenTokenContract.getPastVotes(voter.address, 8062411);
+  if (voterPastVotePower <= 0) {
+    console.log(`Not enough vote power at given block`);
+    throw "Program exiting";
   }
 
   console.log("Voting on proposal");
-  const voteTx = await tokenizedBallotContract.vote(voteIndex.toString(), MIN_VOTE_POWER_VALUE);
+  const voteTx = await tokenizedBallotContract.vote(voteIndex, voteAmount);
   await voteTx.wait();
 
 }
--- scripts/CheckVotingPower.ts
+++ scripts/CheckVotingPower.ts
@@ -1,37 +1,27 @@
 import { ethers } from "hardhat";
-import { GroupTenToken__factory } from "../typechain-types";
+import { GroupTenToken__factory, TokenizedBallot__factory } from "../typechain-types";
 import * as dotenv from 'dotenv'
-dotenv.config()
 
+dotenv.config()
 
 async function main() {
   const alchemyProvider = new ethers.providers.AlchemyProvider("goerli", process.env.ALCHEMY_API_KEY);
   const wallet = new ethers.Wallet(process.env.PRIVATE_KEY ?? "", alchemyProvider);
-  const voter = wallet.connect(alchemyProvider);
+  const signer = wallet.connect(alchemyProvider);
   const tokenContractAddress = process.env.CONTRACT_GTET ?? ""
-  const ballotContractAddress = process.env.CONTRACT_BALLOT ?? ""
   if (tokenContractAddress === undefined || tokenContractAddress === '') {
     throw "make sure CONTRACT_GTET address is set in the .env file";
   }
-  if (ballotContractAddress === undefined || ballotContractAddress === '') {
-    throw "make sure CONTRACT_BALLOT address is set in the .env file";
-  }
-
   const args = process.argv;
   const params = args.slice(2);
-  if (params.length <= 1) throw new Error("arguments are missing");
+  if (params.length <= 0) throw new Error("Please make sure are needed arguments are provided!");
   const voterAddress = params[0];
-  const tokenizedBallotContractBlock = params[1];
 
-  const groupTenTokenContractFactory = new GroupTenToken__factory(voter);
+  const groupTenTokenContractFactory = new GroupTenToken__factory(signer);
   const groupTenTokenContract = groupTenTokenContractFactory.attach(tokenContractAddress);
 
-  let voterVotePower = await groupTenTokenContract.getVotes(voterAddress);
-  let voterPastVotePower = await groupTenTokenContract.getPastVotes(voterAddress, tokenizedBallotContractBlock);
-
-  console.log(`The voter has overall ${ethers.utils.formatEther(voterVotePower)} decimals of vote Power`)
-  console.log(`The voter has ${ethers.utils.formatEther(voterPastVotePower)} vote power in block ${tokenizedBallotContractBlock}`)
-
+  const voterVotePower = await groupTenTokenContract.getVotes(voterAddress);
+  console.log(`The voter has ${ethers.utils.formatEther(voterVotePower)} vote power`)
 }
 
 main().catch((error) => {
