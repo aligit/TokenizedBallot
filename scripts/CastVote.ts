8bb08f114d3301e41a324f2f5214bb873487b454
alifakheri <alifakheri@protonmail.com>
2022-12-03 08:22:53 +0100

    Cleans CastVote to work properly

--- scripts/CastVote.ts
+++ scripts/CastVote.ts
@@ -10,34 +10,19 @@ dotenv.config()
 
 async function main() {
   const args = process.argv;
-  const voteIndex = parseInt(args[2]);
-  const voteAmount = ethers.utils.parseEther(args[3]);
-  console.log(`voteIndex=${voteIndex}`)
-  console.log(`voteAmount=${ethers.utils.formatEther(voteAmount)}`)
-  const ballotContractAddress = process.env.CONTRACT_BALLOT;
-  const tokenContractAddress = process.env.CONTRACT_GTET;
+  const params = args.slice(2);
+  const ballotContractAddress = params[0];
+  const voteIndex = parseInt(params[1]);
+  const voteAmount = ethers.utils.parseEther(params[2]);
   const alchemyProvider = new ethers.providers.AlchemyProvider("goerli", process.env.ALCHEMY_API_KEY);
   const wallet = new ethers.Wallet(process.env.PRIVATE_KEY ?? "", alchemyProvider);
   const voter = wallet.connect(alchemyProvider);
   if (!isNumber(voteIndex)) {
     throw "Only a number representing a proposal name is accepted";
   }
-  // if (!isNumber(voteAmount)) {
-  //   throw "Only a number for vote amount is accepted";
-  // }
   if (ballotContractAddress === undefined || ballotContractAddress === '') {
     throw "make sure CONTRACT_BALLOT address is set in the .env file";
   }
-  if (tokenContractAddress === undefined || tokenContractAddress === '') {
-    throw "make sure CONTRACT_BALLOT address is set in the .env file";
-  }
-
-  console.log(`attaching token Contract`);
-  const groupTenTokenContract = new ethers.Contract(
-    tokenContractAddress,
-    groupTenTokenABI,
-    voter
-  );
 
   console.log(`attaching Ballot Contract`);
   const tokenizedBallotContract = new ethers.Contract(
@@ -54,15 +39,9 @@ async function main() {
     throw "Program exiting";
   }
 
-  const voterPastVotePower = await groupTenTokenContract.getPastVotes(voter.address, 8062411);
-  if (voterPastVotePower <= 0) {
-    console.log(`Not enough vote power at given block`);
-    throw "Program exiting";
-  }
-
-  console.log("Voting on proposal");
   const voteTx = await tokenizedBallotContract.vote(voteIndex, voteAmount);
   await voteTx.wait();
+  console.log("The vote has been casted");
 
 }
 
